a89bcec4f81f097f4b3efb6cc103c6061c739f2a
yango <o3.yang@hotmail.com>
2022-09-11 14:09:47 +0100

    1st commit

---
+++ irisDemoTest.py
@@ -0,0 +1,22 @@
+# Usage
+# irisDemoTest.py
+
+# Load libraries
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn.metrics import classificatoin_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree  import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussionNB
+from sklearn.svm import SVC
+
+url = "./iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+
